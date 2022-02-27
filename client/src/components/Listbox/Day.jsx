993ab182fe3b8f550e8b26083d9ed73ee38b49c6
Ivan Kazanovskiy <ivkazanovskiy@gmail.com>
2022-02-27 13:19:28 +0300

    dev(trainerProfile) released react query logic

--- client/src/components/Listbox/Day.jsx
+++ client/src/components/Listbox/Day.jsx
@@ -1,28 +1,25 @@
 import React, { memo, useState, useEffect } from 'react';
-import { addZero } from '../../helpers/addZero'
 
-function Day({ day, month, year, changeDays, savedHours }) {
-  const currentDay = addZero(year, month, day)
 
-  const [wasSelected, setWasSelected] = useState(false)
-  const [isClicked, setIsClicked] = useState(false)
-  useEffect(() => {
-    setWasSelected(savedHours.map(hour => hour.date).includes(currentDay))
-    setIsClicked(savedHours.map(hour => hour.date).includes(currentDay))
-  }, [savedHours])
+function Day({ date, changeDays, isMarked }) {
 
-  const deletedStyle = "p-2 text-center text-sm text-white border border-gray-300 rounded-lg bg-red-500 "
+  const [isClicked, setIsClicked] = useState(isMarked)
+  // useEffect(() => {
+  //   setWasSelected(savedHours.map(hour => hour.date).includes(currentDay))
+  //   setIsClicked(savedHours.map(hour => hour.date).includes(currentDay))
+  // }, [savedHours])
+
+  // const deletedStyle = "p-2 text-center text-sm text-white border border-gray-300 rounded-lg bg-red-500 "
   const passiveStyle = "p-2 text-center text-sm text-gray-500 border border-gray-300 rounded-lg"
   const activeStyle = "p-2 text-center text-sm text-white border border-gray-300 rounded-lg bg-blue-500 "
 
   return (
     <button onClick={() => {
-      setWasSelected(true)
-      changeDays(currentDay);
+      changeDays(date)
       setIsClicked(!isClicked)
     }
-    } className={isClicked ? activeStyle : (wasSelected) ? deletedStyle : passiveStyle} >
-      {day}
+    } className={isClicked ? activeStyle : passiveStyle} >
+      {date.split('-')[2]}
     </ button >
   );
 }
