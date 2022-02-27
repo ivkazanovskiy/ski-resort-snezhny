7d980fb8a0602a5c757bf4e8589bc4785f0f9127
Kate <ekaterina.s.konovalova@gmail.com>
2022-02-27 10:06:10 +0300

    dev(db): update seeders

---
+++ server/db/seeders/20220226150217-add-orders-data.js
@@ -0,0 +1,98 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Orders', [
+      {
+        start: '2022-02-27',
+        finish: '2022-02-29',
+        roomId: 1,
+        userId: 5,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        start: '2022-02-28',
+        finish: '2022-02-29',
+        roomId: 2,
+        userId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        start: '2022-03-01',
+        finish: '2022-03-04',
+        roomId: 4,
+        userId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        start: '2022-02-27',
+        finish: '2022-02-28',
+        roomId: 46,
+        userId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        start: '2022-02-28',
+        finish: '2022-03-01',
+        roomId: 16,
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        start: '2022-02-27',
+        finish: '2022-02-28',
+        roomId: 10,
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        start: '2022-02-27',
+        finish: '2022-02-28',
+        roomId: 22,
+        userId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        start: '2022-02-27',
+        finish: '2022-02-28',
+        roomId: 23,
+        userId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        start: '2022-02-27',
+        finish: '2022-02-28',
+        roomId: 37,
+        userId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        start: '2022-02-27',
+        finish: '2022-02-28',
+        roomId: 26,
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        start: '2022-03-03',
+        finish: '2022-03-04',
+        roomId: 6,
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Orders', null, {});
+  },
+};
