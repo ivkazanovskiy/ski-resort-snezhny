ab431bdac97ea785ca091c73c36ad50069d57092
Kate <ekaterina.s.konovalova@gmail.com>
2022-02-28 21:48:56 +0300

    dev(search): add slider

---
+++ client/src/components/Elements/Slider.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useQuery, useMutation, useQueryClient } from 'react-query'
+import { Transition } from '@headlessui/react'
+
+function Slider({ type }) {
+
+  const infoQuery = useQuery(`room-${type}`, () => axios(`/api/types/${type}`));
+  const [photos, setPhotos] = useState([]);
+
+  const [isShowing, setIsShowing] = useState(false)
+
+  let info;
+  let relativePath;
+  if (infoQuery.isSuccess) {
+    info = infoQuery.data.data;
+    relativePath = `/rooms/${info.images}`;
+  }
+
+  useEffect(() => {
+    axios({
+      url: '/api/photos/',
+      method: 'GET',
+      headers: {
+        folder: relativePath,
+      },
+    })
+      .then((res) => {
+        setPhotos(res.data.photos);
+      })
+      .catch(err => console.log(err));
+  }, [relativePath]);
+
+  return (
+    <>
+      <div className="flex overflow-x-auto gap-6 snap-x snap-mandatory before:shrink-0 before:w-[30vw] after:shrink-0 after:w-[30vw]">
+        {
+          photos.length ?
+            photos.map(el =>
+              <img key={el} className="shrink-0 snap-center h-96 w-auto" src={`${relativePath}/${el}`} />
+            )
+            :
+            <></>
+        }
+      </div>
+    </>
+  );
+}
+
+export default Slider;
