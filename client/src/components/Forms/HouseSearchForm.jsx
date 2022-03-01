898044cc1fbc105032a47ebb53821492555a0db9
Ivan Kazanovskiy <ivkazanovskiy@gmail.com>
2022-03-01 15:49:23 +0300

    dev(css) start

--- client/src/components/Forms/HouseSearchForm.jsx
+++ client/src/components/Forms/HouseSearchForm.jsx
@@ -28,38 +28,35 @@ function HouseSearchForm(props) {
       <div className="w-full max-w-md py-4">
 
         <Tab.Group>
-          <Tab.List className="flex p-1 space-x-1 bg-blue-900/20 rounded-xl">
+          <Tab.List className="slider">
             <Tab
               className={({ selected }) =>
                 classNames(
-                  'w-full py-2.5 text-sm leading-5 font-medium text-blue-700 rounded-lg',
-                  'focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60',
+                  'slider-tab',
                   selected
-                    ? 'bg-white shadow'
-                    : 'text-blue-100 hover:bg-white/[0.12] hover:text-white'
+                    ? 'slider-active'
+                    : 'slider-passive'
                 )
               } >
               Номера</Tab>
             <Tab
               className={({ selected }) =>
-                classNames(
-                  'w-full py-2.5 text-sm leading-5 font-medium text-blue-700 rounded-lg',
-                  'focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60',
-                  selected
-                    ? 'bg-white shadow'
-                    : 'text-blue-100 hover:bg-white/[0.12] hover:text-white'
-                )
-              }>Коттеджи</Tab>
+              classNames(
+                'slider-tab',
+                selected
+                  ? 'slider-active'
+                  : 'slider-passive'
+              )
+            } >Коттеджи</Tab>
             <Tab
               className={({ selected }) =>
-                classNames(
-                  'w-full py-2.5 text-sm leading-5 font-medium text-blue-700 rounded-lg',
-                  'focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60',
-                  selected
-                    ? 'bg-white shadow'
-                    : 'text-blue-100 hover:bg-white/[0.12] hover:text-white'
-                )
-              } >Гостиницы</Tab>
+              classNames(
+                'slider-tab',
+                selected
+                  ? 'slider-active'
+                  : 'slider-passive'
+              )
+            } >Гостиницы</Tab>
           </Tab.List>
           <Tab.Panels className="mt-2 rounded-md">
             <Tab.Panel className={classNames(
