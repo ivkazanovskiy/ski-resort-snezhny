4febf9f0a968ab8ad761b81afb1841d628c25cf2
Kate <ekaterina.s.konovalova@gmail.com>
2022-03-02 20:12:47 +0300

    dev(login): update login UI

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -42,43 +42,36 @@ function Login(props) {
     setIsCorrectPassword(isValidPassword(password.current.value))
   }
 
-
   return (
-    <form onSubmit={login} className="w-96">
-      <div className="mb-2">
-        <label htmlform="email" className="block mb-2 text-sm font-medium text-gray-900">E-mail</label>
-        <input ref={email} onChange={checkEmail} name="email" type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="name@mail.com" required="" />
-        {(isCorrectEmail) ?
-          <span className="block mb-2 text-sm font-medium text-green-500 ">✓</span>
-          :
-          <span className="block mb-2 text-sm font-medium text-red-500 ">Некорректеный e-mail</span>
-        }
+    <form onSubmit={login} className="card flex-col mb-2 mt-8">
+      <div className="mb-2 flex flex-col">
+        <div className="flex flex-row justify-around justify-items-center items-center">
+          <label htmlFor="email" className="basis-2/6 edit-label text-center">Email</label>
+          <input ref={email} onChange={checkEmail} name="email" type="email" id="email" className="basis-4/6 edit-input border-[1px] border-white/0" />
+        </div>
       </div>
-      <div className="mb-2">
-        <label htmlform="password" className="block mb-2 text-sm font-medium text-gray-900 ">Пароль</label>
-        <input name="password" ref={password} onChange={checkPassword} type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required="" />
-        {(isCorrectPassword) ?
-          <span className="block mb-2 text-sm font-medium text-green-500 ">✓</span>
-          :
-          <span className="block mb-2 text-sm font-medium text-red-500 ">Заглавные и строчные латинские буквы и цифры от 3 до 20</span>
-        }
+      <div className="mb-2 flex flex-col">
+        <div className="flex flex-row justify-around justify-items-center items-center">
+          <label htmlFor="password" className="basis-2/6 edit-label text-center">Пароль</label>
+          <input ref={password} onChange={checkPassword} name="password" type="password" id="password" className={`basis-4/6 edit-input border-[1px] border-white/0`} />
+        </div>
       </div>
-      <div id="secretKey" className="flex items-center mb-2 gap-2 h-8">
-        {/* FIXME: плывет ширина маркера при включении*/}
-        <Switch
-          checked={enabled}
-          onChange={setEnabled}
-          className={`${enabled ? 'bg-blue-600' : 'bg-gray-200'
-            } relative inline-flex items-center h-6 rounded-full w-11`}>
-          <span className="sr-only">Enable notifications</span>
-          <span
-            className={`${enabled ? 'translate-x-6' : 'translate-x-1'
-              } inline-block w-4 h-4 transform bg-white rounded-full`}
-          />
-        </Switch>
-        <div className="text-sm font-medium text-gray-900 " >Войти как инструктор</div>
+      <div className="flex flex-row justify-around justify-items-center items-center">
+        <label className="edit-label basis-2/6 text-center">Войти как инструктор</label>
+        <div className="basis-4/6">
+          <Switch
+            checked={enabled}
+            onChange={setEnabled}
+            className={`${enabled ? 'bg-custom-blue/60' : 'bg-custom-gray/60'}
+          relative inline-flex flex-shrink-0 h-[24px] w-[48px] border-2 border-transparent rounded-lg cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}>
+            <span
+              aria-hidden="true"
+              className={`${enabled ? 'translate-x-6' : 'translate-x-0'}
+            pointer-events-none inline-block h-[20px] w-[20px] rounded-lg bg-white transform ring-0 transition ease-in-out duration-200`} />
+          </Switch>
+        </div>
       </div>
-      <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">Войти</button>
+      <button type="submit" className="px-4 py-2 my-2 text-white bg-custom-blue font-medium rounded-lg text-base w-full text-center">Войти</button>
     </form>
   );
 }
