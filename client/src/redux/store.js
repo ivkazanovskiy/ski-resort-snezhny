575d624098200969985fcc66e4fe46b0b4fc5e52
Ivan Kazanovskiy <ivkazanovskiy@gmail.com>
2022-03-08 21:01:17 +0300

    deploy(package.json) commented devtools

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux'
 import { rootReducer } from './reducers/rootReducer'
 import createSagaMiddleware from 'redux-saga'
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import rootSaga from './saga'
 
 const sagaMiddleware = createSagaMiddleware()
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)))
+export const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+// export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)))
 
 sagaMiddleware.run(rootSaga)
